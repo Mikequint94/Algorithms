564964fa87d8cd7239ef35b82fd9f3dae98c4368
Michael Quint <mjq8@cornell.edu>
2017-10-20 10:15:17 -0700

    quick sort

--- Heaps/michael_quint/lib/heap_sort.rb
+++ Heaps/michael_quint/lib/heap_sort.rb
@@ -2,6 +2,17 @@ require_relative "heap"
 
 class Array
   def heap_sort!
-    heap = BinaryMinHeap.new(Proc.new { |el1, el2| (el1 <=> el2) })
+
+    prc = Proc.new { |el1, el2| (el1 <=> el2) }
+    heap = BinaryMinHeap.new(&prc)
+    self.each do |num|
+      heap.push(num)
+    end
+
+    length.times do |idx|
+      self[idx] = heap.extract
+    end
+
+    self
   end
 end
--- Heaps/michael_quint/lib/k_largest_elements.rb
+++ Heaps/michael_quint/lib/k_largest_elements.rb
@@ -2,4 +2,16 @@ require_relative 'heap'
 
 def k_largest_elements(array, k)
 
+    prc = Proc.new { |el1, el2| (el2 <=> el1) }
+    heap = BinaryMinHeap.new(&prc)
+
+    array.each do |num|
+      heap.push(num)
+    end
+
+    array.length.times do |idx|
+      array[idx] = heap.extract
+    end
+    results = array
+    return results[(array.length-k)..-1]
 end
